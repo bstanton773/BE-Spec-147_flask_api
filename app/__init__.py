bbe75a4e6287214a3d7dcbeb9cef89e33ef5c3c2
Brian <bstanton773@gmail.com>
2024-07-01 16:35:36 -0500

    Database setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
+import os
 from flask import Flask # Import the Flask class from the flask library
+from app.database import db # Import the instance of SQLAlchemy (db) from database module
 
 
 # Create an instance of the flask application
 app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///advanced_ecommerce.db'
 
+# Initialize the app with the flask-sqlalchemy
+db.init_app(app)
 
 # Import the routes file so that it runs
 from . import routes
