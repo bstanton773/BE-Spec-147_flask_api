8e11f24979fe6c1618ee155dbd470b6f05b9e301
Brian <bstanton773@gmail.com>
2024-07-02 15:45:47 -0500

    Update customer routes to read/write to database

--- app/routes.py
+++ app/routes.py
@@ -2,46 +2,32 @@ from app import app # from the app folder, import the app variable (Flask instan
 from flask import request
 from app.schemas.customerSchema import customer_input_schema, customer_output_schema, customers_schema
 from marshmallow import ValidationError
+from app.database import db
+from app.models import Customer
+from werkzeug.security import generate_password_hash
 
 @app.route('/')
 def index():
     return 'Goodbye'
-
-
-customers = [
-    {
-        "id": 1,
-        "first_name": "Brian",
-        "last_name": "Stanton",
-        "username": "brians",
-        "email": "brians@codingtemple.com",
-        "password": "abc123",
-    },
-    {
-        "id": 2,
-        "first_name": "Ryan",
-        "last_name": "Rhoades",
-        "username": "ryanr",
-        "email": "ryanr@codingtemple.com",
-        "password": "cba321"
-    },
-]
 
 # Customer Routes
 
 # Get all customers
 @app.route('/customers', methods=['GET'])
 def get_all_customers():
+    query = db.select(Customer)
+    customers = db.session.scalars(query).all()
     return customers_schema.jsonify(customers)
 
 
 # Get a single customer by ID
 @app.route('/customers/<int:customer_id>', methods=["GET"])
 def get_single_customer(customer_id):
-    # Search for a customer with that ID
-    for customer in customers:
-        if customer['id'] == customer_id:
-            return customer_output_schema.jsonify(customer)
+    # Search the database for a customer with that ID
+    customer = db.session.get(Customer, customer_id)
+    # Check if we get a customer back or None
+    if customer is not None:
+        return customer_output_schema.jsonify(customer)
     return {"error": f"Customer with ID {customer_id} does not exist"}, 404 # Not Found
 
 
@@ -56,10 +42,24 @@ def create_customer():
         data = request.json
         # Check if the body has all of the required fields
         customer_data = customer_input_schema.load(data)
-        # Add the data (with a new id) to the customers list
-        customer_data["id"] = len(customers) + 1
-        customers.append(customer_data)
-        return customer_output_schema.jsonify(customer_data), 201 # Created - Success
+        # Query the customer table to see if any customers have that username or email
+        query = db.select(Customer).where( (Customer.username == customer_data['username']) | (Customer.email == customer_data['email']) )
+        check_customers = db.session.scalars(query).all()
+        if check_customers: # If there are customers in the check_customers list (empty list evaluates to False)
+            return {"error": "Customer with that username and/or email already exists"}, 400 # Bad Request by Client
+        # Create a new instance of Customer 
+        new_customer = Customer(
+            first_name=customer_data['first_name'],
+            last_name=customer_data['last_name'],
+            username=customer_data['username'],
+            email=customer_data['email'],
+            password=generate_password_hash(customer_data['password'])
+        )
+        # and add to the database
+        db.session.add(new_customer)
+        db.session.commit()
+        # Serialize the new customer object and return with 201 status
+        return customer_output_schema.jsonify(new_customer), 201 # Created - Success
     except ValidationError as err:
         return err.messages, 400
     except ValueError as err:
