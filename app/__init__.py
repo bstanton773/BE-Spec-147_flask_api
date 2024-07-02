5c2b184740f6d44ac072788a29fdfe1d324123e8
Brian <bstanton773@gmail.com>
2024-07-02 11:37:50 -0500

    Add Customer model, add Flask-Migrate, and create first migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask # Import the Flask class from the flask library
-from app.database import db # Import the instance of SQLAlchemy (db) from database module
+from app.database import db, migrate # Import the instance of SQLAlchemy (db) and instance of Migrate (migrate) from database module
 
 
 # Create an instance of the flask application
@@ -11,5 +11,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///advanced_ecommerce.db'
 # Initialize the app with the flask-sqlalchemy
 db.init_app(app)
 
+# Initialize the app and db with migrate
+migrate.init_app(app, db)
+
 # Import the routes file so that it runs
-from . import routes
+from . import routes, models
